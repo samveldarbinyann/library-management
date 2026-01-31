7b5b3f352b7756e9625d53d972b3c920b9279cdc
Samvel Darbinyan <s.darbinyan204@gmail.com>
2026-02-01 01:18:11 +0400

    library0.1v

--- src/main/java/com/example/librarymanagementsystem/repository/MemberRepository.java
+++ src/main/java/com/example/librarymanagementsystem/repository/MemberRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Optional<Member> findByEmail(String email);
     Member findById(int id);
 }
